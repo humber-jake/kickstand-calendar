d1873014492c4e86f25b8e2a25f7d0ceca0aab6c
humber-jake <humber.jake@gmail.com>
2024-12-09 01:42:04 -0800

    styling and hardcode color classes for recurring shifts

--- src/Calendar.jsx
+++ src/Calendar.jsx
@@ -17,17 +17,47 @@ const Calendar = (props) => {
     currentMonth[Math.floor(currentMonth.length / 2)]
   ).join(" ");
 
+  // TODO: write function to apply shift classes (MWFS = OttP, Thurs = WTQ)
+
   let calendar = currentMonth.map((day, i) => {
     let { weekday, month, date, year } = day;
+    let classes = ["day"];
+
+    switch (weekday) {
+      case "Monday":
+        classes.push("OttP");
+        break;
+      case "Wednesday":
+        classes.push("OttP");
+        break;
+      case "Friday":
+        classes.push("OttP");
+        break;
+      case "Sunday":
+        classes.push("OttP");
+        break;
+      case "Thursday":
+        classes.push("WTQ");
+        break;
+    }
+
+    if (!midMonth.includes(month || date || year)) {
+      classes.push("extraneous");
+    }
+
+    if (`${month} ${date} ${year}` == today) {
+      classes.push("today");
+    }
 
     return (
       <div
         className={
-          !midMonth.includes(month || date || year)
-            ? "day extraneous"
-            : `${month} ${date} ${year}` == today
-            ? "day today"
-            : "day"
+          classes.join(" ")
+          // !midMonth.includes(month || date || year)
+          //   ? "day extraneous"
+          //   : `${month} ${date} ${year}` == today
+          //   ? "day today"
+          //   : "day"
         }
         key={i}
       >
